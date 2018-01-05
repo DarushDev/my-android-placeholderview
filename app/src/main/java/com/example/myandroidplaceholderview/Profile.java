40294d82f8336e54e59f97b1bc9942761305e5b7
DarushDev <darushfaqiri@gmail.com>
2018-01-05 23:05:37 +0800

    Code Profile model

---
+++ app/src/main/java/com/example/myandroidplaceholderview/Profile.java
@@ -0,0 +1,60 @@
+package com.example.myandroidplaceholderview;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by Darush on 1/5/2018.
+ */
+
+public class Profile {
+
+    @SerializedName("name")
+    @Expose
+    private String name;
+
+    @SerializedName("url")
+    @Expose
+    private String imageUrl;
+
+    @SerializedName("age")
+    @Expose
+    private Integer age;
+
+    @SerializedName("location")
+    @Expose
+    private String location;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+}
---
+++ app/src/main/java/com/example/myandroidplaceholderview/Utils.java
@@ -0,0 +1,15 @@
+package com.example.myandroidplaceholderview;
+
+import android.provider.ContactsContract;
+
+import java.util.List;
+
+/**
+ * Created by Darush on 1/5/2018.
+ */
+
+public class Utils {
+
+    private static final String TAG = "Utils";
+
+}
